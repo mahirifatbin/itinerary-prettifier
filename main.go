925e468b45aab58daa51b576cec037b7319939cc
rifatbinmahi1 <mahi.rifatbin@gmail.com>
2026-01-03 18:20:55 +0200

    WhiteSpace

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -28,7 +29,7 @@ func main() {
 	airportLookup := flag.Arg(2)
 
 	checkFileExist(input, airportLookup)
-
+	//CSV file opening and validation
 	fileOutput, err := openFiles(airportLookup)
 	if err != nil {
 		fmt.Fprint(os.Stderr, "Airport lookup not found")
@@ -38,6 +39,44 @@ func main() {
 	if !validCSV(fileOutput) {
 		fmt.Fprint(os.Stderr, "Airport lookup malformed")
 		os.Exit(1)
+	}
+
+	//Input file reading
+	fileInput, err := inputTextRead(input)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Input not found")
+		os.Exit(1)
+	}
+
+	//trim or clean kora input data store kora
+
+	var trimmedLines []string // new cleaned lines store korar jonno
+	lastLineEmpty := false
+	//fileInput onekgulo sentencer er slice or talika r line hoilo eikhane ekta sentence (string)
+	for _, line := range fileInput {
+
+		trimmed := strings.TrimSpace(line) //whitespace gulo remove korbe
+
+		//advance method use kora lagbe emon kono ulta palta jinish khoj korar jonno-normal kaj korar jonno strings package use korai enough
+		replaceChar := trimmed
+		replaceChar = strings.ReplaceAll(replaceChar, "\r", "\n")
+		replaceChar = strings.ReplaceAll(replaceChar, "\v", "\n")
+		replaceChar = strings.ReplaceAll(replaceChar, "\f", "\n")
+
+		for _, singleLine := range strings.Split(replaceChar, "\n") {
+
+			//multiple empty line check
+			if singleLine == "" && lastLineEmpty {
+				continue
+			}
+			if singleLine == "" {
+				lastLineEmpty = true
+			} else {
+				lastLineEmpty = false
+			}
+			//empty space check
+			trimmedLines = append(trimmedLines, singleLine)
+		}
 	}
 }
 
@@ -110,4 +149,31 @@ func validCSV(reader [][]string) bool {
 
 	}
 	return true
+}
+
+//input text reading
+
+func inputTextRead(path string) ([]string, error) {
+
+	fileTxt, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer fileTxt.Close()
+
+	scanner := bufio.NewScanner(fileTxt)
+
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+
 }
