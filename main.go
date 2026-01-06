3e1a5ff56998f3fbc99fb74df395fc6762d57671
rifatbinmahi1 <mahi.rifatbin@gmail.com>
2026-01-06 16:19:28 +0200

    all input trimmed

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -49,13 +50,16 @@ func main() {
 	}
 
 	//trim or clean kora input data store kora
+	//STEP 6 : input text clean kora
 
 	var trimmedLines []string // new cleaned lines store korar jonno
 	lastLineEmpty := false
+	re := regexp.MustCompile(`[ \t]+`) //multiple space or tab remove korar jonno regexp.loop er baire rakhsi karon bar bar banate hobe na
+
 	//fileInput onekgulo sentencer er slice or talika r line hoilo eikhane ekta sentence (string)
 	for _, line := range fileInput {
 
-		trimmed := strings.TrimSpace(line) //whitespace gulo remove korbe
+		trimmed := strings.TrimSpace(line) //samne and pichoner whitespace gulo remove korbe. majher gulo nah
 
 		//advance method use kora lagbe emon kono ulta palta jinish khoj korar jonno-normal kaj korar jonno strings package use korai enough
 		replaceChar := trimmed
@@ -63,21 +67,27 @@ func main() {
 		replaceChar = strings.ReplaceAll(replaceChar, "\v", "\n")
 		replaceChar = strings.ReplaceAll(replaceChar, "\f", "\n")
 
+		//regexp diye multiple space ba trim jodi thake easily remove kora jabe
+
 		for _, singleLine := range strings.Split(replaceChar, "\n") {
 
+			cleaningMultiSpace := re.ReplaceAllString(singleLine, " ")
+			cleaningMultiSpace = strings.TrimSpace(cleaningMultiSpace)
+
 			//multiple empty line check
-			if singleLine == "" && lastLineEmpty {
+			if cleaningMultiSpace == "" && lastLineEmpty {
 				continue
 			}
-			if singleLine == "" {
+			if cleaningMultiSpace == "" {
 				lastLineEmpty = true
 			} else {
 				lastLineEmpty = false
 			}
 			//empty space check
-			trimmedLines = append(trimmedLines, singleLine)
+			trimmedLines = append(trimmedLines, cleaningMultiSpace)
 		}
 	}
+	//SETEP 6 ENGIND
 }
 
 func checkFileExist(input string, airportLookup string) {
