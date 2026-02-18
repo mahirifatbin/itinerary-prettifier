50c037c34e671deb08a7b8efa515b1007b66cc9d
rifatbinmahi1 <mahi.rifatbin@gmail.com>
2026-02-18 10:51:43 +0200

    dateTransform added

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func init() {
@@ -87,13 +88,20 @@ func main() {
 				lastLineEmpty = false
 			}
 			//empty space check
-			trimmedLines = append(trimmedLines, cleaningMultiSpace)
+			trimmedLines = append(trimmedLines, cleaningMultiSpace) // slice er jonno append use korte hoy. String er jonno noy.
 		}
 	}
 	//SETEP 6 ENGIND
 
 	//Step 7: Transforming IATA & ICAO codes to Airport Names
-	trasnferResult := transformCodeToName(trimmedLines, iata_return, icao_return, city_return)
+	transferResult := transformCodeToName(trimmedLines, iata_return, icao_return, city_return)
+
+	//Step 8 : date Transform er jonno amar ekta alada function lagbe jeta transferResult theke paoa slice k range korle amra string pabw and oi string er moddhe date time pattern khujbo and dateTransform function call kore replace kore dibo.
+	var finalResult []string
+	for _, line := range transferResult {
+		finalResult = append(finalResult, dateTransform(line)) //date transform er result final result e rakha
+	}
+
 	//output file writing
 	fileOut, err := os.Create(output)
 	if err != nil {
@@ -102,11 +110,14 @@ func main() {
 	}
 	defer fileOut.Close()
 	writingResult := bufio.NewWriter(fileOut)
-	for _, outputResultWriting := range trasnferResult {
+	for _, outputResultWriting := range finalResult {
 
 		writingResult.WriteString(outputResultWriting + "\n") //WriteString Ram a joma kore rakhe
 	}
 	writingResult.Flush() // RAM a joma kora data disk e store kore
+
+	//important: main function a main kokhono kichu return korbe nah.
+
 }
 
 func checkFileExist(input string, airportLookup string) {
@@ -229,7 +240,7 @@ func buildLookupMaps(data [][]string) (iataMap map[string]string, icaoMap map[st
 
 }
 
-var pattern = `(#|\##|\*#)([A-Z]{3,4})` // golang er google docs a
+var pattern = `(\*#|##|#)([A-Z]{3,4})` // golang er google docs a
 
 func transformCodeToName(inputText []string, iataMap map[string]string, icaoMap map[string]string, cityMap map[string]string) []string {
 
@@ -246,17 +257,94 @@ func transformCodeToName(inputText []string, iataMap map[string]string, icaoMap 
 
 			if prefix == "#" {
 				result = iataMap[code]
+
 			} else if prefix == "##" {
 				result = icaoMap[code]
+
 			} else if prefix == "*#" {
-				result = cityMap[code]
+				result = cityMap[code] //fullmatch er first 2 char *# remove kore baki ta city code
 			}
 			if result != "" {
-				line = strings.ReplaceAll(line, fullMatch, result) // line theke jeta match korse sheta replace kore dibe result diye
+				line = strings.Replace(line, fullMatch, result, 1) // line theke jeta match korse sheta replace kore dibe result diye. 1 means first match ta replace korbe
 			}
 		}
 		outputLines = append(outputLines, line)
 	}
 	return outputLines
+
+}
+
+var (
+	timePattern       = regexp.MustCompile(`(D|T12|T24)\(([^)]+)\)`) //D(DD MMM YYYY) T12(HH:MM AM/PM) T24(HH:MM)
+	offsetTimePattern = regexp.MustCompile(`(Z|[+-]\d{2}:\d{2})$`)
+)
+
+func dateTransform(inputTime string) string {
+	catchTime := timePattern.FindAllStringSubmatch(inputTime, -1)
+	workingStringTime := inputTime
+	//RFC3339 format: 2006-01-02T15:04:05Z07:00 eita diye just emon format e support korbe but amder input onujai kokhono second nao thakte pare tai amader golan k format bujaiya dite hobe j ki ki format expected from input.
+
+	//time er layout ekta slice a rakha
+	timeLayout := []string{
+		"time.RFC3339",              // etar moddhe shob ache time second soho shob. but jodi second na thake tahole error dibe tai amader alada layout o dite hobe jate second na thakleo parse korte pare.
+		"2006-01-02T15:04:05Z07:00", // professional format holo boro format ta first a rakha
+		"2006-01-02T15:04Z07:00",    // without seconds
+
+	}
+
+	for _, match := range catchTime {
+
+		fullDateMatch := match[0] //D(2007-04-05T12:30-02:00)
+		tag := match[1]           //D|T12|T24
+		rawValue := match[2]      //2007-04-05T12:30-02:00 (D case)	14:45 (T12 / T24 case)
+
+		if !offsetTimePattern.MatchString(rawValue) {
+			continue // offset pattern match na korle continue kore next match e jabe. karon amader expected input e date time er sheshe always offset thakbe. jodi na thake ba bhul thake tahole sheta valid input hobe na.
+		}
+
+		var parsedTime time.Time
+		var err error
+		success := false
+
+		//for parsing
+		for _, layout := range timeLayout {
+			parsedTime, err = time.Parse(layout, rawValue) //raw value layout onujai parse korbe.
+			if err == nil {
+				success = true
+				break // parsing successful hole loop theke ber hoye jabe
+			}
+		}
+		//ei jaygata ektu tricky. success starting a var false and jodi err nil hoy tahole eita true and break kore loop theke ber hoye jabe r jodi !nil hoy tahole !success true hoye jabe and abar continue kore next match a khujte jabe. Its called Gaurd Clause.
+		if !success {
+			continue // jodi kono layout e parse na hoy tahole continue kore next match e jabe
+		}
+
+		//offset finding and adjusting
+		offsetString := parsedTime.Format("-02:00")
+		if offsetString == "Z" || strings.HasSuffix(rawValue, "Z") {
+			offsetString = "+00:00"
+		}
+
+		var result string
+		switch tag {
+		case "D":
+			result = parsedTime.Format("01 Jan 2006")
+
+		case "T12":
+			// parsedTime, err := time.Parse("15:04", rawValue) //time.parse (layout , value). input hisebe amader 15:00 ashte pare but output hisebe amader 03:00 PM dekhate hobe. tai layout e 15:04 dite hobe. karon time.parse er layout e 15:04 but output amar 12H format a dibe jokhon ami format set kore dibw tokhon.
+
+			result = parsedTime.Format("03:04 PM")
+
+		case "T24":
+
+			result = parsedTime.Format("15:04")
+		}
+		if result != "" {
+			workingStringTime = strings.Replace(workingStringTime, fullDateMatch, result, 1) //input time theke jeta match korse sheta replace kore dibe result diye. 1 means first match ta replace korbe
+		}
+		fmt.Println("Before:", inputTime)
+		fmt.Println("After:", workingStringTime)
+	}
+	return workingStringTime
 
 }
